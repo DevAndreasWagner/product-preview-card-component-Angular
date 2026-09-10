b579e6bcbc0ec319d1b85c0e841e3f21b42a5f50
Andreas Wagner <kontakt@andreaswagner-amheuberg.de>
2026-09-10 21:05:36 +0200

    added data into card.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Card } from './components/card/card';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [Card],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './card.scss',
   templateUrl: './card.html',
 })
-export class Card {}
+export class Card {
+  category: string = 'Parfume'
+  title:string = 'Gabrielle Essence Eu De Parfume';
+  description:string ='A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.';
+  oldPrice:number = 169.99;
+  newPrice:number = 149.99;
+}
