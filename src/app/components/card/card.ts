ef84fb0c2727795dc5165b61697b2b8a50c34703
Andreas Wagner <kontakt@andreaswagner-amheuberg.de>
2026-09-16 23:13:48 +0200

    finished responive design, deletet imgSrc in card.ts, added media query

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -9,9 +9,8 @@ import { Button } from '../button/button';
 })
 export class Card {
   category: string = 'PERFUME'
-  title:string = 'Gabrielle Essence Eu De Parfume';
-  description:string ='A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.';
-  oldPrice:number = 169.99;
-  newPrice:number = 149.99;
-  imgSrc:string='assets/images/image-product-desktop.jpg';
+  title: string = 'Gabrielle Essence Eu De Parfume';
+  description: string = 'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.';
+  oldPrice: number = 169.99;
+  newPrice: number = 149.99; 
 }
